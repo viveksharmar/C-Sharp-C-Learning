d6d17b5b10b3e7657160796f2e0f13fe21119565
viveksharmar <146196605+viveksharmar@users.noreply.github.com>
2024-05-14 18:26:36 +0530

    Add files via upload

---
+++ FizzBuzzGame/FizzBuzzGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FizzBuzz Game\n\n");
+            Console.Write("Enter the ending number(where you want to stop the game):");
+            int range = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= range; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
