298858a1ddedeb7f2c7d5c92ed35436ed12fa98a
viveksharmar <146196605+viveksharmar@users.noreply.github.com>
2024-05-14 18:29:18 +0530

    Add files via upload

---
+++ MathsQuiz/MathsQuiz/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, marks = 0;
+            Console.WriteLine("Maths Quiz of 10 questions(1 mark each)\n\n");
+
+            while (i <= 10)
+            {
+                Random rnd = new Random();
+                int num1 = Convert.ToInt32(rnd.Next(0, 200));
+                int num2 = Convert.ToInt32(rnd.Next(0, 200));
+                int num3 = Convert.ToInt32(rnd.Next(0, 15));
+                int rndchoice = Convert.ToInt32(rnd.Next(1, 5));
+                Console.WriteLine("\nQuestion no.:"+i+"\n\n");
+                i++;
+
+                switch (rndchoice)
+                {
+                    case (1):
+                        Console.WriteLine(num1);
+                        Console.WriteLine(num2);
+                        Console.Write("Add the given two numbers:");
+                        int sum = Convert.ToInt32(Console.ReadLine());
+
+                        if (sum == (num1 + num2))
+                        {
+                            Console.WriteLine("Correcto!");
+                            marks++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Oh ho!, The correct result is " + (num1 + num2));
+                        }
+                        break;
+
+                    case (2):
+                        Console.WriteLine(num1);
+                        Console.WriteLine(num3);
+                        Console.Write("Multiply the given two numbers:");
+                        int multi = Convert.ToInt32(Console.ReadLine());
+
+                        if (multi == (num1 * num3))
+                        {
+                            Console.WriteLine("Correcto!");
+                            marks++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Oh ho!, The correct result is " + (num1 * num3));
+                        }
+                        break;
+
+                    case (3):
+                        Console.WriteLine(num1);
+                        Console.WriteLine(num2);
+                        Console.Write("Difference the given two numbers:");
+                        int sub = Convert.ToInt32(Console.ReadLine());
+
+                        if ((sub == (num1 - num2)) || (sub == (num2 - num1)))
+                        {
+                            Console.WriteLine("Correcto!");
+                            marks++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Oh ho!, The correct result is " + Math.Abs(num1 - num2));
+                        }
+                        break;
+
+                    case (4):
+                        Console.WriteLine(num1);
+                        Console.WriteLine(num3);
+                        Console.Write("Remainder if abover number divided by below one:");
+                        int rem = Convert.ToInt32(Console.ReadLine());
+
+                        if (rem == (num1 % num3))
+                        {
+                            Console.WriteLine("Correcto!");
+                            marks++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Oh ho!, The correct result is " + (num1 % num3));
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine("\n\nScored Marks:"+marks);
+            if(marks == 10)
+            {
+                Console.WriteLine("Excellent");
+            }
+            else if(marks == 9) 
+            {
+                Console.WriteLine("Good");
+            }
+            else if(marks == 7 || marks == 8)
+            {
+                Console.WriteLine("Fair");
+            }
+            else if(marks >=0 && marks < 7)
+            {
+                Console.WriteLine("Need to improve yourself!");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong in assigning marks");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PalindromeCheck/PalindromeCheck/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to check it is palindrome:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int temp = num;
+
+            int rev = 0, rem;
+
+            while (num > 0)
+            {
+                rem = num % 10;
+
+                rev = (rev * 10) + rem;
+
+                num /= 10;
+            }
+
+
+            if (temp == rev)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
